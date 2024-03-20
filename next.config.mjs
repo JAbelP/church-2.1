59725bb154411c4117fe281eaefb19b22fa490c7
JAbelP <abelpinales97@gmail.com>
2024-03-20 00:13:33 -0400

    First Page Check (Add ENV)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "a-us.storyblok.com",
+        port: "",
+      },
+    ],
+  },
+};
+
+export default withNextIntl(nextConfig);
